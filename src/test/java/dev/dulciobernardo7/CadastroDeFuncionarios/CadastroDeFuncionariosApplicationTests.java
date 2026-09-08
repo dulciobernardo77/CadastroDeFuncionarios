1ed1f2ca8827e85d651f225b6fef9d7a913e0a9b
dulciobernardo77 <dulciobernardo40@gmail.com>
2026-09-08 17:38:43 +0100

    test: adicionar regressão para sexo do formulário

--- src/test/java/dev/dulciobernardo7/CadastroDeFuncionarios/CadastroDeFuncionariosApplicationTests.java
+++ src/test/java/dev/dulciobernardo7/CadastroDeFuncionarios/CadastroDeFuncionariosApplicationTests.java
@@ -1,13 +1,22 @@
 package dev.dulciobernardo7.CadastroDeFuncionarios;
 
+import dev.dulciobernardo7.CadastroDeFuncionarios.Controller.DTO.PessoaDTO;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 @SpringBootTest
 class CadastroDeFuncionariosApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void pessoaDtoShouldAllowEmptySexoValueForFormBinding() {
+		PessoaDTO pessoa = new PessoaDTO();
+		assertNull(pessoa.getSexo());
 	}
 
 }
