5e88265d653b0c255ce96471938fb1cd686606d2
dulciobernardo77 <dulciobernardo40@gmail.com>
2026-09-10 15:41:10 +0100

    refactor: converter DTOs para records
    Converte PessoaDTO e TarefasDTO para records, adicionando construtores vazios para uso nos formularios.

--- src/main/java/dev/dulciobernardo7/CadastroDeFuncionarios/Controller/DTO/PessoaDTO.java
+++ src/main/java/dev/dulciobernardo7/CadastroDeFuncionarios/Controller/DTO/PessoaDTO.java
@@ -1,25 +1,24 @@
 package dev.dulciobernardo7.CadastroDeFuncionarios.Controller.DTO;
 
 import dev.dulciobernardo7.CadastroDeFuncionarios.Entity.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-@Builder
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-public class PessoaDTO {
 
-    private Long id;
-    private String nome ;
-    private int idade;
-    private String nacionalidade;
-    private String bi;
-    private Character sexo;
-    private String email;
-    private String telefone;
-    private String nivel;
-    private String imgUrl;
-    private TarefasModel tarefa;
+import lombok.Builder;
+
+@Builder
+public record PessoaDTO (
+     Long id,
+     String nome,
+     int idade,
+     String nacionalidade,
+     String bi,
+     Character sexo,
+     String email,
+     String telefone,
+     String nivel,
+     String imgUrl,
+     TarefasModel tarefa) {
+
+     public PessoaDTO() {
+          this(null, null, 0, null, null, null, null, null, null, null, null);
+     }
 }
--- src/main/java/dev/dulciobernardo7/CadastroDeFuncionarios/Controller/DTO/TarefasDTO.java
+++ src/main/java/dev/dulciobernardo7/CadastroDeFuncionarios/Controller/DTO/TarefasDTO.java
@@ -1,22 +1,17 @@
 package dev.dulciobernardo7.CadastroDeFuncionarios.Controller.DTO;
 
 import dev.dulciobernardo7.CadastroDeFuncionarios.Entity.*;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import java.util.List;
 
 @Builder
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-public class TarefasDTO {
+public record TarefasDTO (
+    Long id,
+    String nomeDaTarefa,
+    String dificuldade,
+    List<PessoaModel> pessoas){
 
-    private Long id;
-    private String nomeDaTarefa;
-    private String dificuldade;
-    private List<PessoaModel> pessoas;
-
+    public TarefasDTO() {
+        this(null, null, null, null);
+    }
 }
