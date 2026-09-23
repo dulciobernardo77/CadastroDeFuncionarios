5a0ae65112b39d640128321d4e859f5d8b07db48
dulciobernardo77 <dulciobernardo40@gmail.com>
2026-09-23 23:49:54 +0100

    feat: handle validation errors globally #18

--- src/main/java/dev/dulciobernardo7/CadastroDeFuncionarios/Config/AplicationControllerAdvice.java
+++ src/main/java/dev/dulciobernardo7/CadastroDeFuncionarios/Config/AplicationControllerAdvice.java
@@ -1,4 +1,27 @@
 package dev.dulciobernardo7.CadastroDeFuncionarios.Config;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
 public class AplicationControllerAdvice {
+
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String,String> handleArgumentNotValideionException(MethodArgumentNotValidException ex){
+        Map<String,String> errors = new HashMap<>();
+
+        ex.getBindingResult().getAllErrors().forEach( (error) ->
+                errors.put(((FieldError) error).getField(),
+                        error.getDefaultMessage()));
+        return errors;
+    }
 }
