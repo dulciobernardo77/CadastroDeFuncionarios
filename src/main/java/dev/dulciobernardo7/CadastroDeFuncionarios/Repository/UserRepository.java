053c944c2657941084a1ab3a819e93e1c4430c34
dulciobernardo77 <dulciobernardo40@gmail.com>
2026-09-25 15:49:15 +0100

    Atualiza repositorio de utilizadores

--- src/main/java/dev/dulciobernardo7/CadastroDeFuncionarios/Repository/UserRepository.java
+++ src/main/java/dev/dulciobernardo7/CadastroDeFuncionarios/Repository/UserRepository.java
@@ -1,4 +1,14 @@
 package dev.dulciobernardo7.CadastroDeFuncionarios.Repository;
 
-public class UserRepository {
+import dev.dulciobernardo7.CadastroDeFuncionarios.Entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
 }
