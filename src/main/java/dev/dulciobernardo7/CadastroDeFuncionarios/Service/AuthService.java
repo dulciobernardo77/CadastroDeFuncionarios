c04dcdf91a11a35d660e92fee4ae90603dbd2f5b
dulciobernardo77 <dulciobernardo40@gmail.com>
2026-09-25 15:49:15 +0100

    Atualiza servico de autenticacao

--- src/main/java/dev/dulciobernardo7/CadastroDeFuncionarios/Service/AuthService.java
+++ src/main/java/dev/dulciobernardo7/CadastroDeFuncionarios/Service/AuthService.java
@@ -1,4 +1,20 @@
 package dev.dulciobernardo7.CadastroDeFuncionarios.Service;
 
-public class AuthService {
+import dev.dulciobernardo7.CadastroDeFuncionarios.Repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuario ou senha incorreta"));
+    }
 }
